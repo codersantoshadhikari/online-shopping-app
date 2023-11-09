d37ddf03f351b9478612e51b0f7a61f4614f4385
codersantoshadhikari <santoshadhikari.name.np@gmail.com>
2023-11-09 21:58:52 +0545

    E-commerce

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'config/theme/my_theme.dart';
+import 'app/data/local/my_shared_pref.dart';
+import 'app/routes/app_pages.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await MySharedPref.init();
+  runApp(
+    ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      rebuildFactor: (old, data) => true,
+      useInheritedMediaQuery: true,
+      builder: (context, widget) {
+        return GetMaterialApp(
+          useInheritedMediaQuery: true,
+          title: "E-commerce",
+          debugShowCheckedModeBanner: false,
+          builder: (context, widget) {
+            bool themeIsLight = MySharedPref.getThemeIsLight();
+            return Theme(
+              data: MyTheme.getThemeData(isLight: themeIsLight),
+              child: MediaQuery(
+                data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+                child: widget!,
+              ),
+            );
+          },
+          initialRoute: AppPages.INITIAL,
+          getPages: AppPages.routes,
+        );
+      },
+    ),
+  );
+}
